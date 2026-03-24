91e9ddea572ef579bf3861a012b69b8d91b68851
Metin Serez <metin@serezart.com>
2026-03-25 01:06:18 +0300

    Update publishers.py

--- publishers.py
+++ publishers.py
@@ -1,7 +1,6 @@
 import requests
 import base64
 
-# --- WORDPRESS AYARLARI ---
 WP_URL = "https://felsefemiz.net/wp-json/wp/v2/posts"
 WP_USER = "serezart"
 WP_APP_PASS = "TBTJ w0hn 9Pz7 FyIa A6py xj6O"
@@ -11,20 +10,8 @@ def send_to_wordpress(title, content):
     token = base64.b64encode(credentials.encode()).decode()
     headers = {'Authorization': f'Basic {token}', 'Content-Type': 'application/json'}
     payload = {'title': title, 'content': content, 'status': 'publish'}
-
     try:
         response = requests.post(WP_URL, json=payload, headers=headers)
         return response.status_code == 201, response.text
     except Exception as e:
         return False, str(e)
-
-def send_to_telegram(bot, chat_id, text, photo=None):
-    try:
-        if photo:
-            bot.send_photo(chat_id, photo, caption=text)
-        else:
-            bot.send_message(chat_id, text)
-        return True
-    except Exception as e:
-        print(f"Telegram Hatası: {e}")
-        return False
