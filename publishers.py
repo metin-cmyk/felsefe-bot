7a8bd93f428a197d3fc00c79096a617b2ab9c0e7
Metin Serez <metin@serezart.com>
2026-03-25 01:08:13 +0300

    Update publishers.py

--- publishers.py
+++ publishers.py
@@ -1,17 +1,32 @@
 import requests
 import base64
 
+# WordPress Bilgilerin
 WP_URL = "https://felsefemiz.net/wp-json/wp/v2/posts"
 WP_USER = "serezart"
 WP_APP_PASS = "TBTJ w0hn 9Pz7 FyIa A6py xj6O"
 
 def send_to_wordpress(title, content):
+    """felsefemiz.net sitesine içerik gönderir."""
     credentials = f"{WP_USER}:{WP_APP_PASS}"
     token = base64.b64encode(credentials.encode()).decode()
-    headers = {'Authorization': f'Basic {token}', 'Content-Type': 'application/json'}
-    payload = {'title': title, 'content': content, 'status': 'publish'}
+    
+    headers = {
+        'Authorization': f'Basic {token}',
+        'Content-Type': 'application/json'
+    }
+    
+    payload = {
+        'title': title,
+        'content': content,
+        'status': 'publish'
+    }
+    
     try:
         response = requests.post(WP_URL, json=payload, headers=headers)
-        return response.status_code == 201, response.text
+        if response.status_code == 201:
+            return True, "Başarılı"
+        else:
+            return False, f"Hata: {response.status_code}"
     except Exception as e:
         return False, str(e)
